be01de34466cf94159036443b387c610e4f2d24f
LoganHowey <logan.howey@rocky.edu>
2022-09-19 16:32:43 -0600

    Initial commit

---
+++ src/util/QuickSort.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.Comparator;
+
+public class QuickSort {
+
+    public static <Type> void quickSort(Type[] toSort, Comparator<Type> comp){
+
+    }
+
+    private static <Type> void sort(Type[] toSort, int l, int h){
+        int p;
+        
+    }
+}
